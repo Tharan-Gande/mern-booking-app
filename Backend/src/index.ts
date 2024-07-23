d6f3719cbe0d7ba298826f57f5c9dc52044d76e6
Gande Bhanu Tharan <150318874+Tharan-Gande@users.noreply.github.com>
2024-07-23 14:36:35 +0530

    Update index.ts

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -21,12 +21,12 @@ app.use(
             credentials:true,
         }
     ));
-app.use(express.static(path.join(__dirname,"../../frontend/dist")));
+app.use(express.static(path.join(__dirname,"../../Frontend/dist")));
 
 app.use("/api/auth",authRoutes)
 app.use("/api/users",userRoutes)
 app.get("*", (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../../Frontend/dist/index.html"));
   });
 
 app.listen(7000,()=>{
